b1e52df439c648b24d8b3646fdb5566e85492ce6
aletheia-tech <64600153+aletheia-tech@users.noreply.github.com>
2020-05-04 17:37:45 -0400

    Troubleshooting SSL

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: [
   	require('tailwindcss', './tailwind.config.js'),
-  	require('autoprefixer'),
+    require('autoprefixer'),
+    // RUN WHEN READY TO PUBLISH
+    //  require('@fullhuman/postcss-purgecss')({
+    //    content: [
+    //      "./_site/**/*.html"
+    //    ],
+    //      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    //  })
   ]
 }
